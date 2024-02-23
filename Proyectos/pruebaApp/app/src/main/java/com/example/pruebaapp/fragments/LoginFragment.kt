0f56cd9a3a0bfe41bc0c4d65b55e7fab9486002c
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-02-22 23:14:27 -0500

    FIREBASE y Problema de emulación solventado con package controllers, notificaciones mostrandose en interfaz

--- Proyectos/pruebaApp/app/src/main/java/com/example/pruebaapp/fragments/LoginFragment.kt
+++ Proyectos/pruebaApp/app/src/main/java/com/example/pruebaapp/fragments/LoginFragment.kt
@@ -18,12 +18,6 @@ import com.example.pruebaapp.databinding.FragmentLoginBinding
 //        viewBinding true
 //    }
 //} private lateinit var binding: ResultProfileBinding
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
     //Binding en fragments
     private var _binding : FragmentLoginBinding? = null
