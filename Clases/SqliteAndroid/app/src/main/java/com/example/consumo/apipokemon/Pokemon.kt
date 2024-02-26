d03c0c9642bc3b6d87beffc6df3774094956f7b5
velasco182 <ruben.velasco0@misena.edu.co>
2024-02-26 18:46:22 -0500

    Consumo Pokemon

--- Clases/SqliteAndroid/app/src/main/java/com/example/consumo/apipokemon/Pokemon.kt
+++ Clases/SqliteAndroid/app/src/main/java/com/example/consumo/apipokemon/Pokemon.kt
@@ -2,7 +2,8 @@ package com.example.consumo.apipokemon
 
 data class Pokemon(
     val id: Int,
-    val name: String
+    val name: String,
+    //val url : String
 )
 
 
--- Clases/SqliteAndroid/app/src/main/java/com/example/consumo/apipokemon/PokemonAdapter.kt
+++ Clases/SqliteAndroid/app/src/main/java/com/example/consumo/apipokemon/PokemonAdapter.kt
@@ -3,14 +3,17 @@ package com.example.consumo.apipokemon
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.consumo.R
+import com.squareup.picasso.Picasso
 
 class PokemonAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
 
     class PokemonViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvPokemonName: TextView = view.findViewById(R.id.tvPokemonName)
+        val ivPokemon : ImageView = view.findViewById(R.id.ivPokemon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
@@ -20,6 +23,7 @@ class PokemonAdapter(private val pokemonList: List<Pokemon>) : RecyclerView.Adap
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
         val pokemon = pokemonList[position]
+        Picasso.get().load("https://i.pinimg.com/originals/04/5a/6b/045a6bf929075321781c3b4805b0dbfc.png").into(holder.ivPokemon)
         holder.tvPokemonName.text = pokemon.name
     }
 
